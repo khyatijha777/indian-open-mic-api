3780f313b74e0fdb8acff53604d3b79a54fdbf6e
Keshav Kabra <keshavkabra@keshav-2.local>
2025-05-03 18:23:27 +0530

    fix: main file

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,26 @@ import * as dotenv from 'dotenv';
 import * as express from 'express';
 
 async function bootstrap() {
+  // Load environment variables
   dotenv.config();
   console.log('Loaded Environment Variables:');
   Object.keys(process.env).forEach((key) => {
     console.log(`${key} = ${process.env[key]}`);
   });
 
+  // Disable Nest's built-in body parser
   const app = await NestFactory.create(AppModule, {
-    bodyParser: true, // default is true anyway
-    rawBody: false,
+    bodyParser: false,
   });
-  app.enableCors();
 
-  console.log('app: ', app);
-  // // Set global body size limits using Express parser options
+  // Use custom Express parsers with increased limits
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+  // Enable CORS
+  app.enableCors();
+
+  // Apply global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -29,6 +32,7 @@ async function bootstrap() {
     }),
   );
 
+  // Start server
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
