427e0ed11155cec8454bb31dff2f302e81136e71
Keshav Kabra <keshavkabra@keshav-2.local>
2025-05-03 19:09:47 +0530

    fix: implementation

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as express from 'express';
 
 async function bootstrap() {
-  // Load environment variables
   dotenv.config();
   console.log('Loaded Environment Variables:');
   Object.keys(process.env).forEach((key) => {
     console.log(`${key} = ${process.env[key]}`);
   });
 
-  // Disable Nest's built-in body parser
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: true,
-  });
-
-  // Use custom Express parsers with increased limits
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-  // Enable CORS
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // Apply global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -32,7 +20,6 @@ async function bootstrap() {
     }),
   );
 
-  // Start server
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
