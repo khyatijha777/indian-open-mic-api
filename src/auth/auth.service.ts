ab4e539e92c3eddc25e326935c9bc6ee56f549c6
khyati-pencilz <khyati@pencilz.io>
2025-05-03 11:19:00 +0530

    chore: user auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException ,BadRequestException} from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/login.dto';
@@ -18,9 +22,9 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    console.log("user: ", user);
+    console.log('user: ', user);
     console.log('Input password:', loginDto.password);
-console.log('Stored password:', user.password);
+    console.log('Stored password:', user.password);
     const isPasswordValid = await compare(loginDto.password, user.password);
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
@@ -29,27 +33,29 @@ console.log('Stored password:', user.password);
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
-      ...user
+      ...user,
     };
   }
 
-  // async signup(createUserDto: CreateUserDto) {
-  //   const existingUser = await this.usersService.findByEmail(createUserDto.email);
-  //   if (existingUser) {
-  //     throw new BadRequestException('Email already in use');
-  //   }
-  //   console.log("SIGN UP createUserDto.password: ", createUserDto.password);
-    
-  //   const hashedPassword = await hash(createUserDto.password, 10);
-  //   const user = await this.usersService.create({
-  //     ...createUserDto,
-  //     password: hashedPassword,
-  //   });
+  async signup(createUserDto: CreateUserDto) {
+    const existingUser = await this.usersService.findByEmail(
+      createUserDto.email,
+    );
+    if (existingUser) {
+      throw new BadRequestException('Email already in use');
+    }
+    console.log('SIGN UP createUserDto.password: ', createUserDto.password);
 
-  //   const payload = { email: user.email, sub: user.id };
-  //   return {
-  //     access_token: this.jwtService.sign(payload),
-  //     ...user,
-  //   };
-  // }
+    const hashedPassword = await hash(createUserDto.password, 10);
+    const user = await this.usersService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      ...user,
+    };
+  }
 }
